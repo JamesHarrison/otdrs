9de5bd1f879bf124e43b1741c2154136adc6ba32
James Harrison <james@talkunafraid.co.uk>
2025-07-31 19:12:03 +0000

    correct upload path, PEP-257 attribute docs

--- otdrs.pyi
+++ otdrs.pyi
@@ -17,166 +17,157 @@ class MapBlock:
 class GeneralParametersBlock:
     """General details about this trace"""
 
-    # Language code - EN, CN, JP, etc.
     language_code: str
-    # Cable identifier
+    """Language code - EN, CN, JP, etc."""
     cable_id: str
-    # Fibre identifier
+    """Cable identifier"""
     fiber_id: str
-    # Fibre type - this is generally coded as the ITU-T standard definition
+    """Fibre identifier"""
     fiber_type: int
-    # Nominal test wavelength in nm
+    """Fibre type - this is generally coded as the ITU-T standard definition"""
     nominal_wavelength: int
-    # Start location for the test
+    """Nominal test wavelength in nm"""
     originating_location: str
-    # End location for the test
+    """Start location for the test"""
     terminating_location: str
-    # Cable code - free field
+    """End location for the test"""
     cable_code: str
-    #  NC for new condition, RC for as-repaired, OT as something else
+    """Cable code - free field"""
     current_data_flag: str
-    # User offset - This is essentially the launch lead length from the front panel offset (provided in the fixed parameters block), in 100ps increments
+    """ NC for new condition, RC for as-repaired, OT as something else"""
     user_offset: int
-    # This is the same as user_offset, but measured in 10x the distance units of FixedParametersBlock.units_of_distance
+    """User offset - This is essentially the launch lead length from the front panel offset (provided in the fixed parameters block), in 100ps increments"""
     user_offset_distance: int
-    # Operator of the unit for the test
+    """This is the same as user_offset, but measured in 10x the distance units of FixedParametersBlock.units_of_distance"""
     operator: str
-    # Free comment field
+    """Operator of the unit for the test"""
     comment: str
+    """Free comment field"""
 
 class SupplierParametersBlock:
     """Details about the OTDR used in the test"""
 
-    # Manufacturer of the OTDR
     supplier_name: str
-    # Mainframe model number
+    """Manufacturer of the OTDR"""
     otdr_mainframe_id: str
-    # Mainframe serial number
+    """Mainframe model number"""
     otdr_mainframe_sn: str
-    # Optical module model number
+    """Mainframe serial number"""
     optical_module_id: str
-    # Optical module serial number
+    """Optical module model number"""
     optical_module_sn: str
-    # Software revision
+    """Optical module serial number"""
     software_revision: str
-    # Free text
+    """Software revision"""
     other: str
+    """Free text"""
 
 class FixedParametersBlock:
     """Details about the trace that don't vary between datasets"""
 
-    # Datestamp - unix epoch seconds, 32-bit. Remember not to do any OTDR
-    # tests after 2038.
     date_time_stamp: int
-    # Units of distance - km, mt, ft, kf, mi, etc. Typically mt (in civilised
-    # nations)
+    """Datestamp - unix epoch seconds, 32-bit. Remember not to do any OTDR tests after 2038."""
     units_of_distance: str
-    # Actual wavelength used - normally the factory-calibrated wavelength in
-    # nm, or nominal wavelength
+    """Units of distance - km, mt, ft, kf, mi, etc. Typically mt (in civilised nations)"""
     actual_wavelength: int
-    # Acquisition offset - this is the length of fibre from the OTDR port to
-    # the first data point in the DataPoints, in 100ps increments
+    """Actual wavelength used - normally the factory-calibrated wavelength in nm, or nominal wavelength"""
     acquisition_offset: int
-    # As acquisition_offset, but as 10x units_of_distance
+    """Acquisition offset - this is the length of fibre from the OTDR port to the first data point in the DataPoints, in 100ps increments"""
     acquisition_offset_distance: int
-    # The total number of pulse widths used, if more than one pulse width's
-    # results are stored in the file
+    """As acquisition_offset, but as 10x units_of_distance"""
     total_n_pulse_widths_used: int
-    # The list of pulse widths used, in nanoseconds
+    """The total number of pulse widths used, if more than one pulse width's results are stored in the file"""
     pulse_widths_used: list[int]
-    # Data spacing, time taken to acquire 10,000 points in 100ps increments
+    """The list of pulse widths used, in nanoseconds"""
     data_spacing: list[int]
-    # Number of points stored for each pulse width
+    """Data spacing, time taken to acquire 10,000 points in 100ps increments"""
     n_data_points_for_pulse_widths_used: list[int]
-    # Group index - the refractive index of the fibre, default to 146800 if
-    # nothing supplied
+    """Number of points stored for each pulse width"""
     group_index: int
-    # Backscatter coefficient -
+    """Group index - the refractive index of the fibre, default to 146800 if nothing supplied"""
     backscatter_coefficient: int
-    # Number of averages - the number of samples that were averaged to
-    # generate the result - may be used instead of averaging time
+    """Backscatter coefficient -"""
     number_of_averages: int
-    # Averaging time - may be supplied instead of number of averages - in
-    # seconds x 10
+    """Number of averages - the number of samples that were averaged to generate the result - may be used instead of averaging time"""
     averaging_time: int
-    # Acquisition range set by the tester to reach the end of the fibre - as
-    # with other distance measurements, 100ps increments
+    """Averaging time - may be supplied instead of number of averages - in seconds x 10"""
     acquisition_range: int
-    # Acquisition range in 10x distance units, as an alternate to
-    # acquisition_range
+    """Acquisition range set by the tester to reach the end of the fibre - as with other distance measurements, 100ps increments"""
     acquisition_range_distance: int
-    # Front panel offset is the time taken, in 100ps increments, between the
-    # front-end of the optical TRX and the front panel connector
+    """Acquisition range in 10x distance units, as an alternate to acquisition_range"""
     front_panel_offset: int
-    # Noise floor level - the lowest power level for which 98% of the noise
-    # data lies below; 5-digit -dB value (e.g. 10200 = -10.2dB)
+    """Front panel offset is the time taken, in 100ps increments, between the front-end of the optical TRX and the front panel connector"""
     noise_floor_level: int
-    # Scale factor for the noise floor level - defaults to 1
+    """Noise floor level - the lowest power level for which 98% of the noise data lies below; 5-digit -dB value (e.g. 10200 = -10.2dB)"""
     noise_floor_scale_factor: int
-    # Attenuation in dB*1000 applied by the instrument if done by the
-    # instrument
+    """Scale factor for the noise floor level - defaults to 1"""
     power_offset_first_point: int
-    # The threshold in dB*1000 for a loss-type event; default 00200
+    """Attenuation in dB*1000 applied by the instrument if done by the instrument"""
     loss_threshold: int
-    # The threshold in -dB*1000 for reflectance events; default -55000
+    """The threshold in dB*1000 for a loss-type event; default 00200"""
     reflectance_threshold: int
-    # The threshold in dB*1000 for the loss taken to detect the end of the
-    # fibre; default 03000
+    """The threshold in -dB*1000 for reflectance events; default -55000"""
     end_of_fibre_threshold: int
-    # Trace type - identifies if this is a standard one-way trace, a
-    # bidirectional trace, reference trace, difference trace, or reversed
-    # trace
+    """The threshold in dB*1000 for the loss taken to detect the end of the fibre; default 03000"""
     trace_type: str
-    # Window coordinate for the upper right window corner
+    """Trace type - identifies if this is a standard one-way trace, a bidirectional trace, reference trace, difference trace, or reversed trace"""
     window_coordinate_1: int
-    # Power coordinate for the upper right window corner
+    """Window coordinate for the upper right window corner"""
     window_coordinate_2: int
-    # Window coordinate for the lower left window corner
+    """Power coordinate for the upper right window corner"""
     window_coordinate_3: int
-    # Power coordinate for the lower left window corner
+    """Window coordinate for the lower left window corner"""
     window_coordinate_4: int
+    """Power coordinate for the lower left window corner"""
 
 class KeyEvent:
     """Event detected by the OTDR"""
 
-    # Event number - this is from 0 to n
     event_number: int
-    # Event propogation time is the time in 100ps units from the front panel
-    # to the event
+    """Event number - this is from 0 to n"""
+
     event_propogation_time: int
-    # The span loss in db/km (as a 5-digit value, i.e. dB*1000) for the fibre
-    # entering the event
+    """Event propogation time is the time in 100ps units from the front panel to the event"""
+
     attenuation_coefficient_lead_in_fiber: int
-    # Loss in dB*1000 for the event
+    """The span loss in db/km (as a 5-digit value, i.e. dB*1000) for the fibre entering the event"""
+
     event_loss: int
-    # Reflectance in -dB*1000 for the event
+    """Loss in dB*1000 for the event"""
+
     event_reflectance: int
-    # Code for the event is a 6-byte string:
-    # Byte 1:
-    #     0 = nonreflective, 1 = reflective, 2 = saturated reflective
-    # Byte 2:
-    #     A = added by user, M = moved by user, E = end of fibre, F = found
-    #     by software, O = out of range, D = modified end of fibre
-    # Remaining bytes are the Landmark number if used - 9s otherwise
+    """Reflectance in -dB*1000 for the event"""
+
     event_code: str
-    # Loss measurement technique - 2P for two point, LS for least squares, OT
-    # for other
+    """Code for the event is a 6-byte string:
+    Byte 1:
+        0 = nonreflective, 1 = reflective, 2 = saturated reflective
+    Byte 2:
+        A = added by user, M = moved by user, E = end of fibre, F = found
+        by software, O = out of range, D = modified end of fibre
+    Remaining bytes are the Landmark number if used - 9s otherwise
+    """
+
     loss_measurement_technique: str
-    # Marker location - ML1 is the OTDR side for 2P/LS/OT measurements
+    """Loss measurement technique - 2P for two point, LS for least squares, OT for other"""
+
     marker_location_1: int
-    # Marker location - ML2 is the OTDR side for LS measurements, and bounds
-    # the event for 2P/OT
+    """Marker location - ML1 is the OTDR side for 2P/LS/OT measurements"""
+
     marker_location_2: int
-    # Marker location - ML3 is on the far side for LS measurements, and empty
-    # for 2P/OT
+    """Marker location - ML2 is the OTDR side for LS measurements, and bounds the event for 2P/OT"""
+
     marker_location_3: int
-    # Marker location - ML4 is on the far side for LS measurements, and empty
-    # for 2P/OT
+    """Marker location - ML3 is on the far side for LS measurements, and empty for 2P/OT"""
+
     marker_location_4: int
-    # Marker location - ML5 is the reflectance calculation position
+    """Marker location - ML4 is on the far side for LS measurements, and empty for 2P/OT"""
+
     marker_location_5: int
-    # Free comment on the event
+    """Marker location - ML5 is the reflectance calculation position"""
+
     comment: str
+    """Free comment on the event"""
 
 class LastKeyEvent:
     """Like a KeyEvent, but specific to the last event detected"""
@@ -218,12 +209,12 @@ class KeyEvents:
 class DataPointsAtScaleFactor:
     """The data points for a specific scale factor"""
 
-    # Number of points in this block
     n_points: int
-    # Scale factor for the data, as 1000*SF
+    """Number of points in this block"""
     scale_factor: int
-    # Data points as dB*1000
+    """Scale factor for the data, as 1000*SF"""
     data: list[int]
+    """Data points as dB*1000"""
 
 class DataPoints:
     """The data points for this trace"""
@@ -267,8 +258,9 @@ class ProprietaryBlock:
 
 class SORFile:
     """A SOR file.
-    SORFile describes a full SOR file. All blocks except MapBlock are     types as we cannot guarantee the parser will find them, but many blocks a|None
-    in fact mandatory in the specification so compliant files will provide them.
+    SORFile describes a full SOR file. All blocks except MapBlock are optional types as we cannot
+    guarantee the parser will find them, but many blocks are in fact mandatory in the specification,
+    so compliant instruments will provide them.
     """
 
     map: MapBlock
